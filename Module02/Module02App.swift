59c06c5baec7142b7da0963bb8e6f0a7cab4cb12
quietmid <ninjonglu@gmail.com>
2025-09-02 17:09:29 +0300

    Initial Commit

---
+++ Module02/Module02App.swift
@@ -0,0 +1,17 @@
+//
+//  Module02App.swift
+//  Module02
+//
+//  Created by Joseph Lu on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Module02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
